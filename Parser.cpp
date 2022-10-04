ff43fd27f630603afd666d67d24fc86415a7653d
Spencer Baird <suspenceb@gmail.com>
2022-10-03 18:19:36 -0600

    trying to push to second branch

---
+++ Parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by suspe on 10/3/2022.
+//
+
+#include "Parser.h"
---
+++ Parser.h
@@ -0,0 +1,14 @@
+//
+// Created by suspe on 10/3/2022.
+//
+
+#ifndef INC_236PROJECT_1_PARSER_H
+#define INC_236PROJECT_1_PARSER_H
+
+
+class Parser {
+
+};
+
+
+#endif //INC_236PROJECT_1_PARSER_H
